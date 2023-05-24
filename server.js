f0545ee8679935fdf0694271b0b104bdd90c81e1
Sultepronit <sultepronit@gmail.com>
2023-05-24 20:09:56 +0300

    now we can get info from json file

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const PORT = process.env.PORT || 3030;
 
+var goodsDb = {};
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+fs.readFile('./db/goods.json', 'utf8', function(err, data) {
+	goodsDb = JSON.parse(data);
+	console.log(goodsDb);
+});
+
+var server = app.listen(PORT, function() {
+	console.log(`server started on port ${PORT}`);
+});
 
 app.get('/', function(req, res) {
 	res.send('<h1>Hello world!<h1>');
 });
 
-var server = app.listen(PORT, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	//console.log('Example app listening at http://%s:%s', host, port);
-	console.log(`server started on port ${PORT}`);
-});
+
