c4aaee4474c4bcdc779dd1e5172898429e0a792f
Sultepronit <sultepronit@gmail.com>
2023-05-24 22:02:38 +0300

    added new goods, new style, new functionality

--- public/app.js
+++ public/app.js
@@ -1,26 +1,48 @@
 "use strict";
 var goodsData = '';
+var currentShop = '';
 
-function displayCard(src, name, price) {
+function added(n) {
+	console.log(n);
+	console.log(goodsData[currentShop][n]);
+}
+
+function displayCard(src, name, price, id) {
 	var card = '<div class="card"> \
 		<img src="img_src"> \
 		<p class="product-name">product_name</p> \
 		<P class="product-price">product_price</p> \
-		<button class="to-cart">add to cart</button> \
+		<button class="to-cart" onclick="added(##)">add to cart</button> \
 		</div>';
 	
 	var re = card.replace('img_src', src);
 	re = re.replace('product_name', name);
 	re = re.replace('product_price', price + ' uah');
+	re = re.replace('##', id);
 	$('.goods').append(re);
 }
 
-function showGoodsAtStart() {
-	var currentShop = goodsData["Burger Hetman"];
-	//console.log(
-	for(var i = 0; i < currentShop.length; i++) {
-		displayCard('img/' + currentShop[i].img_src, currentShop[i].name, currentShop[i].price);
+function changeShop(shop) {
+	if(currentShop) {
+		$('.goods').empty();
+		shopId = '#' + currentShop;
+		$(shopId).css('background-color', 'inherit');
+		$(shopId).css('color', 'inherit');
 	}
+	currentShop = shop;
+	
+	var shopId = '#' + shop;
+	$(shopId).css('background-color', 'red');
+	$(shopId).css('color', 'white');
+	
+	var goods = goodsData[shop];
+	for(var i = 0; i < goods.length; i++) {
+		displayCard('img/' + goods[i].img_src, goods[i].name, goods[i].price, i);
+	}
+}
+
+function showGoodsAtStart() {
+	changeShop('burger');
 }
 
 function getData() {
@@ -41,12 +63,17 @@ function getData() {
 var main = function() {	
 	getData();
 	
-	/*for(var i = 0; i < 3; i ++) {
-		displayCard('img/burger_0.jpg', 'Burger sushi style');
-	}
-	for(var i = 0; i < 3; i ++) {
-		displayCard('img/borshch_0.png', 'Fantastic borshch');
-	}*/
+	$('#burger').on('click', function() {
+		changeShop('burger');
+	});
+	
+	$('#kurcha').on('click', function() {
+		changeShop('kurcha');
+	});
+	
+	$('#sushi').on('click', function() {
+		changeShop('sushi');
+	});
 }
 
 $(document).ready(main);
