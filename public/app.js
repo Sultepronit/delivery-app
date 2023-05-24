580597630922b2adde7ee443dfcd6b493f266fc7
Sultepronit <sultepronit@gmail.com>
2023-05-24 20:43:05 +0300

    now we sending burgers from db to page!

--- public/app.js
+++ public/app.js
@@ -1,24 +1,52 @@
 "use strict";
+var goodsData = '';
 
-function displayCard(src, name) {
+function displayCard(src, name, price) {
 	var card = '<div class="card"> \
 		<img src="img_src"> \
 		<p class="product-name">product_name</p> \
+		<P class="product-price">product_price</p> \
 		<button class="to-cart">add to cart</button> \
 		</div>';
 	
 	var re = card.replace('img_src', src);
 	re = re.replace('product_name', name);
+	re = re.replace('product_price', price + ' uah');
 	$('.goods').append(re);
 }
 
+function showGoodsAtStart() {
+	var currentShop = goodsData["Burger Hetman"];
+	//console.log(
+	for(var i = 0; i < currentShop.length; i++) {
+		displayCard('img/' + currentShop[i].img_src, currentShop[i].name, currentShop[i].price);
+	}
+}
+
+function getData() {
+	var xhr = new XMLHttpRequest();
+    xhr.open("GET", '/data', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+        	//console.log(xhr.response);
+			goodsData = JSON.parse(xhr.response);
+			console.log(goodsData);
+			showGoodsAtStart();
+        }
+    };
+    try { xhr.send(); } catch (err) {console.log(err) }
+}
+
 var main = function() {	
-	for(var i = 0; i < 3; i ++) {
+	getData();
+	
+	/*for(var i = 0; i < 3; i ++) {
 		displayCard('img/burger_0.jpg', 'Burger sushi style');
 	}
 	for(var i = 0; i < 3; i ++) {
 		displayCard('img/borshch_0.png', 'Fantastic borshch');
-	}
+	}*/
 }
 
 $(document).ready(main);
--- server.js
+++ server.js
@@ -6,22 +6,28 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3030;
 
 var goodsDb = {};
+var goodsJson = '';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 fs.readFile('./db/goods.json', 'utf8', function(err, data) {
-	goodsDb = JSON.parse(data);
-	console.log(goodsDb);
+	goodsJson = data;
+	/*goodsDb = JSON.parse(data);
+	console.log(goodsDb);*/
 });
 
 var server = app.listen(PORT, function() {
 	console.log(`server started on port ${PORT}`);
 });
 
-app.get('/', function(req, res) {
-	res.send('<h1>Hello world!<h1>');
+app.get('*', function(req, res) {
+	console.log(req.url);
+	if(req.url == '/data') {
+		//res.send('<h1>Hello world!<h1>');
+		res.send(goodsJson);
+	}
 });
 
 
