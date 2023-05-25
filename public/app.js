75010245820d6fa2fe6425ae4565908029d7ad3f
Sultepronit <sultepronit@gmail.com>
2023-05-25 09:11:03 +0300

    added style & functionality for cart buttons; ready to create cart itself

--- public/app.js
+++ public/app.js
@@ -1,10 +1,38 @@
 "use strict";
 var goodsData = '';
 var currentShop = '';
+//var goodsInCart = {};
+var goodsInCart = [];
+var countGoodsInCart = 0;
 
 function added(n) {
 	console.log(n);
 	console.log(goodsData[currentShop][n]);
+	
+	if(goodsInCart[n]) { // already in cart!
+		
+	} else { // add to cart!
+		goodsInCart[n] = 1;
+		countGoodsInCart++;
+		console.log(goodsInCart);
+		console.log(countGoodsInCart);
+		
+		$('.counter').text(countGoodsInCart);
+		
+		//change button(s)
+		var id = '#' + n;
+		$(id).text('go to cart');
+		$(id).css('background-color', 'green');
+		$(id).css('border-color', 'green');
+		$(id).css('color', 'white');
+		$(id).css('font-weight', 'bold');
+		
+		//change cart 
+		if(countGoodsInCart == 1) {
+			/*$('#cart').css('color', 'green');*/
+			$('#cart').css('font-weight', 'bold');
+		}
+	}
 }
 
 function displayCard(src, name, price, id) {
@@ -12,13 +40,13 @@ function displayCard(src, name, price, id) {
 		<img src="img_src"> \
 		<p class="product-name">product_name</p> \
 		<P class="product-price">product_price</p> \
-		<button class="to-cart" onclick="added(##)">add to cart</button> \
+		<button class="to-cart" onclick="added(##)" id="##">add to cart</button> \
 		</div>';
 	
 	var re = card.replace('img_src', src);
 	re = re.replace('product_name', name);
 	re = re.replace('product_price', price + ' uah');
-	re = re.replace('##', id);
+	re = re.replace(/##/g, id);
 	$('.goods').append(re);
 }
 
